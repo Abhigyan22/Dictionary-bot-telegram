35cf1abcc4df1c6360083ad33dcf329c5489668c
Abhigyan Kaushik <abhigyankaushik22@gmail.com>
2021-09-04 16:46:29 +0530

    Replit bug

--- keep_alive.py
+++ keep_alive.py
@@ -1,14 +1,18 @@
+# This file is to keep the bot running in replit
 from flask import Flask
 from multiprocessing import Process
 
-app = Flask('')
+app = Flask("")
 
-@app.route('/')
+
+@app.route("/")
 def home():
     return "Hello. I am alive!"
 
+
 def run():
-  app.run(host='0.0.0.0',port=8080)
+    app.run(host="0.0.0.0", port=8080)
+
 
 def keep_alive():
     t = Process(target=run)
